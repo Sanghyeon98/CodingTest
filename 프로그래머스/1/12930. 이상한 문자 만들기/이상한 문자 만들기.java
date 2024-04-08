aa5e8e5ff068f0362eb8ee979744812703e35e0c
박상현 <116146931+Sanghyeon98@users.noreply.github.com>
2024-04-09 00:58:26 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.13 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String s) {
+      
+        String[] words = s.split(" ",-1);
+        
+        StringBuilder sb =new StringBuilder();
+        for(String word  :words){
+            for(int i = 0; i<word.length(); i++){
+                 if(i % 2 == 0){
+                    sb.append(Character.toUpperCase(word.charAt(i)));
+                } else {
+                    sb.append(Character.toLowerCase(word.charAt(i)));
+                }
+            }
+            sb.append(" ");
+        }
+         if(sb.length()>0){
+                sb.deleteCharAt(sb.length() - 1);
+            }
+        
+        return sb.toString();
+    }
+}
